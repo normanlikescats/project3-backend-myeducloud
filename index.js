b0c6c5b83e97cf6e85960d04c32afb3ddfcd91c6
normanlikescats <normankwok97@gmail.com>
2023-04-18 17:14:51 +0800

    added test controller and router, verified all functions working

--- index.js
+++ index.js
@@ -20,30 +20,37 @@ const checkJwt = auth({
 });
 
 const db = require("./db/models/index");
-const { users, questionnaires, chatrooms, messages } = db;
+
+const { users, questionnaires, student_answers, chatrooms, messages, tests } = db;
 
 const UserController = require("./Controllers/UserController");
 const QuestionnaireController = require("./Controllers/QuestionnaireController");
+const AnswerController = require("./Controllers/AnswerController");
+const TestController = require("./Controllers/TestController");
 const MessageController = require("./Controllers/MessageController");
 
 const UserRouter = require("./Routers/UserRouter");
 const QuestionnaireRouter = require("./Routers/QuestionnaireRouter");
+const AnswerRouter = require("./Routers/AnswerRouter");
+const TestRouter = require("./Routers/TestRouter");
 const MessageRouter = require("./Routers/MessageRouter");
 
 const userController = new UserController(users);
 const questionnaireController = new QuestionnaireController(questionnaires);
+const answerController = new AnswerController(student_answers);
+const testController = new TestController(tests);
 const messageController = new MessageController(messages, chatrooms, users);
 
 const userRouter = new UserRouter(userController, express).route();
-const questionnaireRouter = new QuestionnaireRouter(
-  questionnaireController,
-  express
-).route();
-
+const questionnaireRouter = new QuestionnaireRouter(questionnaireController, express).route();
+const answerRouter = new AnswerRouter(answerController, express).route();
+const testRouter = new TestRouter(testController, express).route();
 const messageRouter = new MessageRouter(messageController, express).route();
 
 app.use("/students", userRouter);
 app.use("/questionnaire", questionnaireRouter);
+app.use("/answers", answerRouter);
+app.use("/test", testRouter)
 app.use("/messages", messageRouter);
 
 // Socket IO implementation
