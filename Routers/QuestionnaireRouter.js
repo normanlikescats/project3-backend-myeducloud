b79a380dea1bd2645ff19c13feaaaad06500977a
normanlikescats <normankwok97@gmail.com>
2023-04-25 10:40:52 +0800

    add auth0 to backend routes

--- Routers/QuestionnaireRouter.js
+++ Routers/QuestionnaireRouter.js
@@ -1,16 +1,17 @@
 class QuestionnaireRouter {
-  constructor(questionnaireController, express) {
+  constructor(questionnaireController, express, checkJwt) {
     this.questionnaireController = questionnaireController;
     this.express = express;
+    this.checkJwt = checkJwt
   }
   route = () => {
     let router = this.express.Router();
 
-    router.get("/:testid", this.questionnaireController.getAllQuestions);
-    router.get("/question/:id", this.questionnaireController.getOneQuestion);
-    router.post("/:testid", this.questionnaireController.insertOneQuestion);
-    router.put("/edit/:id", this.questionnaireController.editOneQuestion);
-    router.delete("/delete/:id", this.questionnaireController.deleteOneQuestion);
+    router.get("/:testid", this.checkJwt, this.questionnaireController.getAllQuestions);
+    router.get("/question/:id", this.checkJwt, this.questionnaireController.getOneQuestion);
+    router.post("/:testid", this.checkJwt, this.questionnaireController.insertOneQuestion);
+    router.put("/edit/:id", this.checkJwt, this.questionnaireController.editOneQuestion);
+    router.delete("/delete/:id", this.checkJwt, this.questionnaireController.deleteOneQuestion);
 
     return router;
   };
